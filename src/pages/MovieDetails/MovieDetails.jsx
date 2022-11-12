82e3871b33c69c0c9bc34504d3b5cfca435881d8
Oleksandr <105509345+Oleksandr-Berd@users.noreply.github.com>
2022-11-12 23:22:00 +0200

    Update MovieDetails.jsx

--- src/pages/MovieDetails/MovieDetails.jsx
+++ src/pages/MovieDetails/MovieDetails.jsx
@@ -63,8 +63,8 @@ export const MovieDetails = () => {
         <p className={css.add__infoText}>Additional information:</p>
         <ul className={css.add__info}>
           {NavItemsDetails.map(({ href, text }) => (
-            <li>
-              <Link to={href} key={href} className={css.details__element}>
+            <li key={href}>
+              <Link to={href} className={css.details__element}>
                 {text}
               </Link>
             </li>
